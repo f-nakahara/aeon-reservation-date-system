c40ac666bc1973385d01fcc3fe56a3cb622bd7de
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-12-26 00:38:06 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { OrderController } from "./controller/order-controller";
-import { GMMessage } from "./model/gm-message";
+import { GMMessage } from "./repository/google-mail-repository/model/gm-message";
 import { GoogleMailRepositoryImpl } from "./repository/google-mail-repository/google-mail-repository-impl";
+import { GoogleCalendarRepositoryImpl } from "./repository/google-calendar-repository/google-calendar-repository-impl";
 
 async function main() {
   const controller = new OrderController({
     mailRepository: new GoogleMailRepositoryImpl(),
+    calendarRepository: new GoogleCalendarRepositoryImpl(),
   });
   const order = await controller.getLatestOrder();
-  console.log(order);
+  const today = new Date();
+  let yesterday = new Date(today.toDateString());
+  yesterday.setDate(yesterday.getDate() + 1);
+  if (order.date > yesterday && order.date <= today) {
+    await controller.addToCalendar(order);
+  }
 }
