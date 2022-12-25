5063bbb44369d971618c44094a2839b06d63b8c3
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-12-26 00:00:12 +0900

    Create order-controller.ts

---
+++ src/controller/order-controller.ts
@@ -0,0 +1,63 @@
+import { Order } from "../entity/order";
+import { GoogleMailRepository } from "../repository/google-mail-repository/google-mail-repository";
+
+export class OrderController {
+  mailRepository: GoogleMailRepository;
+
+  constructor(param: { mailRepository: GoogleMailRepository }) {
+    this.mailRepository = param.mailRepository;
+  }
+
+  async getLatestOrder(): Promise<Order> {
+    const threads = await this.mailRepository.search({
+      query: {
+        from: "aeonnetshop@aeonnetshop.com",
+      },
+      start: 0,
+      max: 1,
+    });
+    if (threads.length === 0) {
+      throw new Error();
+    }
+    const messages = threads[0].messages;
+    if (messages.length === 0) {
+      throw new Error();
+    }
+    const message = messages[0];
+    const dateText = this.getValue(message.body, "お届け日時").split(" "); // 2022年 12月 19日 15:00-17:00
+    const order: Order = {
+      id: message.id,
+      date: message.date,
+      deliveryDate: {
+        startTime: new Date(
+          `${dateText[0].split("年")[0]}/${dateText[1].split("月")[0]}/${
+            dateText[2].split("日")[0]
+          } ${dateText[3].split("-")[0].split(":")[0]}:${
+            dateText[3].split("-")[0].split(":")[1]
+          }:00`
+        ),
+        endTime: new Date(
+          `${dateText[0].split("年")[0]}/${dateText[1].split("月")[0]}/${
+            dateText[2].split("日")[0]
+          } ${dateText[3].split("-")[1].split(":")[0]}:${
+            dateText[3].split("-")[1].split(":")[1]
+          }:00`
+        ),
+      },
+      recipient: this.getValue(message.body, "お客さま氏名"),
+      address: this.getValue(message.body, "お届け先住所"),
+      tel: this.getValue(message.body, "お届け先TEL"),
+      store: this.getValue(message.body, "ご対応店舗"),
+      link: threads[0].permaLink,
+      items: [],
+      totalPrice: message.body.split("お支払い総計：")[1].split("\n")[0],
+    };
+    return order;
+  }
+
+  getValue(text: string, key: string): string {
+    let res = text.split(`[${key}] `)[1].split("\n")[0];
+    res = res.replace("\r", "");
+    return res;
+  }
+}
