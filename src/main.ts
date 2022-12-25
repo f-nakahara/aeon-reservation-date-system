96f7b188c354fea6a9d3e6f4559073090140f8c7
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2022-12-26 00:00:14 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-function main() {
-    const threads = GmailApp.search('from:(aeonnetshop@aeonnetshop.com)', 0, 10);
-    console.log(threads);
+import { OrderController } from "./controller/order-controller";
+import { GMMessage } from "./model/gm-message";
+import { GoogleMailRepositoryImpl } from "./repository/google-mail-repository/google-mail-repository-impl";
+
+async function main() {
+  const controller = new OrderController({
+    mailRepository: new GoogleMailRepositoryImpl(),
+  });
+  const order = await controller.getLatestOrder();
+  console.log(order);
 }
